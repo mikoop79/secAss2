cf56cf5156fed6f4c461385d68b751870902b054
pryor365 <sally.pryor@gmail.com>
2013-05-26 17:16:09 +1000

    proposed change to algorithm

--- examples/example3.php
+++ examples/example3.php
@@ -1,2 +1,5 @@
 <h1>Experiment Three </h1>
 <h2>Our Implementation of the Secure Cookie Scheme</h2>
+<p> session id | expiration time | (data)k | HMAC(session id | expiration time | data, k)
+<br/> where k = HMAC(session id | expiration time, sk)</p>
+<p><strong>NOTE</strong> sk = server key </p>
