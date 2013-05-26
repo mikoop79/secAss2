3d24cd084d31578fa826e5eabeafb1c4b17782ba
pryor365 <sally.pryor@gmail.com>
2013-05-26 16:55:11 +1000

    updated description of the Fu's scheme

--- examples/example2.php
+++ examples/example2.php
@@ -1,4 +1,5 @@
 <h1>Experiment Two </h1>
 <h2>Implementation of Fu's Secure Cookie Scheme</h2>
-<p>username | expiration time| (data)k | HMAC(username | expiration time | data , k) <br/> where k = HMAC(username | expiration time, sk)</p>
-<p><strong>NOTE</strong> sk = serverkey</p>
+<p>username | expiration time| data | HMAC(username | expiration time | data , sk) 
+<br/> where sk  is the server key</p>
+<p><strong>Note</strong> the data is sent unencrypted, just part of the cookie value</p>
